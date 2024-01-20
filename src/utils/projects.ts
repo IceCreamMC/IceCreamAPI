2c0e15be2bf5f9e78bc578e4d64bfdb0b98f9288
Pancakse <152734075+Pancaksee@users.noreply.github.com>
2024-01-20 05:14:20 +0000

    work

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -5,14 +5,10 @@ type Project = {
 
 const PROJECTS: Map<string, Project> = new Map();
 
-PROJECTS.set("leaves", {
-    name: "Leaves",
-    repo: "LeavesMC/Leaves"
+PROJECTS.set("icecream", {
+    name: "IceCream",
+    repo: "IceCreamMC/IceCream"
 });
 
-PROJECTS.set("lumina", {
-    name: "Lumina",
-    repo: "LeavesMC/Lumina"
-});
 
 export default PROJECTS;
